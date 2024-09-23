94091bec2bd04d2a673db586322a029be805b55c
kimjinwoo <rlawlsdn177@naver.com>
2024-09-23 21:35:25 +0900

    style: 하단 메뉴 이름 추가 및 스타일 수정정

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,8 +1,17 @@
+//src/components/BottomBar.tsx
 import React from 'react';
-import {View, Image, TouchableOpacity, StyleSheet} from 'react-native';
-
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  Dimensions,
+} from 'react-native';
+const {width} = Dimensions.get('window');
 const BottomBar = () => {
   // Fix the images object with unique keys
+
   const images = {
     note: require('../../assets/images/icons/note.png'),
     study: require('../../assets/images/icons/study.png'),
@@ -15,18 +24,24 @@ const BottomBar = () => {
     <View style={styles.container}>
       <TouchableOpacity style={styles.iconContainer}>
         <Image source={images.note} style={styles.icon} />
+        <Text style={styles.label}>기록장</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.iconContainer}>
         <Image source={images.study} style={styles.icon} />
+        <Text style={styles.label}>스터디</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.iconContainer}>
         <Image source={images.home} style={styles.icon} />
+        <Text style={styles.label}>홈</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.iconContainer}>
         <Image source={images.alarm} style={styles.icon} />
+        <Text style={styles.label}>알림</Text>
       </TouchableOpacity>
+
       <TouchableOpacity style={styles.iconContainer}>
         <Image source={images.profile} style={styles.icon} />
+        <Text style={styles.label}>프로필</Text>
       </TouchableOpacity>
     </View>
   );
@@ -37,18 +52,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    height: 60,
-    width: 400,
+    height: 80,
+    width: width,
     backgroundColor: '#fff',
     borderTopWidth: 1,
     borderTopColor: '#e0e0e0',
   },
   iconContainer: {
     padding: 10,
+    alignItems: 'center',
   },
   icon: {
-    width: 24,
-    height: 24,
+    width: 26,
+    height: 26,
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 12,
+    color: '#ABABAB', // 텍스트 색상
+    fontFamily: 'NanumSquareNeo-eHv', // 커스텀 폰트 적용
   },
 });
 
