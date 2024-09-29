de45a1f593164b665afc8994030ebb1888ff958c
kimjinwoo <rlawlsdn177@naver.com>
2024-09-29 23:43:30 +0900

    chore: merge

--- App.tsx
+++ App.tsx
@@ -10,6 +10,9 @@ import AlarmScreen from './src/screens/AlarmScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import StudyRecordScreen from './src/screens/StudyRecordScreen';
 import FindPassword from './src/screens/FindPasswordScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import ChangePasswordScreen from './src/screens/ChangePasswordScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen';
 
 import FriendsProfile from './src/screens/FriendsProfile';
 const Stack = createStackNavigator();
@@ -28,6 +31,9 @@ const App = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Log" component={StudyRecordScreen} />
         <Stack.Screen name="FindPassword" component={FindPassword} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
         <Stack.Screen name="FriendsProfile" component={FriendsProfile} />
       </Stack.Navigator>
     </NavigationContainer>
