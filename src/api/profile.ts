4a7e61e1c36520bcccf4b94dcfa3b74dcab4cdd9
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-10-08 04:40:33 +0900

    feat: 프로필 api 연결

---
+++ src/api/profile.ts
@@ -0,0 +1,43 @@
+import apiClient from './axiosInstance';
+import {getItem} from './asyncStorage';
+export interface Member {
+  id: number;
+  name: string;
+  profileImage: string | null;
+  mainTitle: string;
+  freezeCount: number;
+}
+
+interface ApiResponse {
+  success: boolean;
+  response: {
+    member: Member;
+  };
+  error: any;
+}
+
+export const getMemberData = async (): Promise<Member | null> => {
+  try {
+    const token = await getItem('authToken');
+    console.log(token);
+    if (!token) {
+      console.error('토큰이 없습니다. 로그인이 필요합니다.');
+      return null;
+    }
+
+    const response = await apiClient.get<ApiResponse>('/members', {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    if (response.data.success) {
+      return response.data.response.member;
+    } else {
+      console.error('API 에러:', response.data.error);
+      return null;
+    }
+  } catch (error) {
+    console.error('네트워크 에러:', error);
+    return null;
+  }
+};
