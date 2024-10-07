e49fa0629c343eb578fb67412e73c9defc42b738
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-10-08 04:39:53 +0900

    feat: 뱃지 api 연결

---
+++ src/api/badge.ts
@@ -0,0 +1,42 @@
+import apiClient from './axiosInstance';
+import {getItem} from './asyncStorage';
+
+export interface Badge {
+  id: number;
+  fileName: string;
+  name: string;
+  description: string;
+}
+
+interface BadgeApiResponse {
+  success: boolean;
+  response: {
+    badgeCount: number;
+    badges: Badge[];
+  } | null;
+  error: any;
+}
+
+export const getBadges = async (id: number): Promise<Badge[] | null> => {
+  try {
+    const token = await getItem('authToken');
+    const response = await apiClient.get<BadgeApiResponse>(
+      `/members/${id}/badges`,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      },
+    );
+
+    if (response.data.success && response.data.response) {
+      return response.data.response.badges;
+    } else {
+      console.error('API 에러:', response.data.error);
+      return null;
+    }
+  } catch (error) {
+    console.error('네트워크 에러:', error);
+    return null;
+  }
+};
