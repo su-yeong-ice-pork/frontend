dc8e875fbd496282e98595c1ce5fd03ff92ef235
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-10-08 01:05:00 +0900

    Signup 연결

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {RecoilRoot} from 'recoil';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LandingScreen from './src/screens/LandingScreen';
@@ -17,24 +18,29 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="FriendsProfile"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Landing" component={LandingScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Study" component={StudyScreen} />
-        <Stack.Screen name="StudyDetail" component={StudyDetailScreen} />
-        <Stack.Screen name="Alarm" component={AlarmScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Log" component={StudyRecordScreen} />
-        <Stack.Screen name="FindPassword" component={FindPassword} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-        <Stack.Screen name="FriendsProfile" component={FriendsProfile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Landing"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Landing" component={LandingScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="Study" component={StudyScreen} />
+          <Stack.Screen name="StudyDetail" component={StudyDetailScreen} />
+          <Stack.Screen name="Alarm" component={AlarmScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Log" component={StudyRecordScreen} />
+          <Stack.Screen name="FindPassword" component={FindPassword} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen
+            name="ChangePassword"
+            component={ChangePasswordScreen}
+          />
+          <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+          <Stack.Screen name="FriendsProfile" component={FriendsProfile} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 };
 
